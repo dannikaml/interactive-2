91a959e49d873999fcdb5acc061c6111ac13a599
dannikaml <dannikaml@hotmail.com>
2023-03-21 19:40:08 -0500

    add folder paths; set schema; add env; add package.json

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+-- DROP DATABASE
+DROP DATABASE IF EXISTS jobs_db;
+
+-- CREATE DATABASE
+CREATE DATABASE jobs_db;
+
+
+
+-- CREATE TABLE jobs (
+--   id INT(11) NOT NULL AUTO_INCREMENT,
+--   title VARCHAR(255) NOT NULL,
+--   description TEXT,
+--   salary DECIMAL(10, 2),
+--   PRIMARY KEY (id)
+-- );
