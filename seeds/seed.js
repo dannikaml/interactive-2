dbb77d4c71c01703f1bbb4c678c97d6aa3ea702c
dannikaml <dannikaml@hotmail.com>
2023-03-23 18:42:45 -0500

    chore clean up schema; add seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,7 +2,9 @@ const sequelize = require('../config/connection');
 const { User, Project } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const seedData = require('./frontEndData.json');
+const seedData = require('./backEndData.json');
+const seedData = require('./fullStackData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
@@ -12,14 +14,14 @@ const seedDatabase = async () => {
     returning: true,
     });
 
-    for (const project of projectData) {
+    for (const project of seedData) {
         await Project.create({
         ...project,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
     }
 
-    process.exit(0);
+process.exit(0);
 };
 
 seedDatabase();
