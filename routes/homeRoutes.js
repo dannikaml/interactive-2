26b74d7ed2061944c222bf7aab167da23b42d416
dannikaml <dannikaml@hotmail.com>
2023-03-24 10:02:27 -0500

    fix server.js; add seeded data; add routes

---
+++ routes/homeRoutes.js
@@ -0,0 +1,80 @@
+const router = require('express').Router();
+const { Jobs, User } = require('../models');
+// const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    const jobsData = await Jobs.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+ 
+    const Jobs = jobsData.map((Jobs) => Jobs.get({ plain: true }));
+
+    res.render('homepage', { 
+      Jobs, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/jobs/:id', async (req, res) => {
+  try {
+    const jobsData = await Jobs.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const Jobs = jobsData.get({ plain: true });
+
+    res.render('jobs', {
+      ...Jobs,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+// router.get('/profile', withAuth, async (req, res) => {
+//   try {
+//     // Find the logged in user based on the session ID
+//     const userData = await User.findByPk(req.session.user_id, {
+//       attributes: { exclude: ['password'] },
+//       include: [{ model: Jobs }],
+//     });
+
+//     const user = userData.get({ plain: true });
+
+//     res.render('profile', {
+//       ...user,
+//       logged_in: true
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// router.get('/login', (req, res) => {
+//   // If the user is already logged in, redirect the request to another route
+//   if (req.session.logged_in) {
+//     res.redirect('/profile');
+//     return;
+//   }
+
+//   res.render('login');
+// });
+
+module.exports = router;
