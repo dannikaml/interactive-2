3e5a97109b35afda23a34680453964a8dbd12bd2
John Klink <john.r.klink92@gmail.com>
2023-03-23 19:05:52 -0500

    discard
    dont push changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,2 @@
-function renderContent() {
-    var source = document.getElementById('content-template').innerHTML;
-    var template = Handlebars.compile(source);
-    var context = getContent();
-    var html = template(context);
-    document.getElementById('content-placeholder').innerHTML = html;
-  }
 
   
