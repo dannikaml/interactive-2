1e54694b14c2c371077664eb2d60ad032f635fa2
dannikaml <dannikaml@hotmail.com>
2023-03-27 20:42:03 -0500

    fix server.js for handlebars; fix homeRoutes for jobPost

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -48,7 +48,7 @@ router.get('/jobs/:id', withAuth, async (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
+router.get('/jobsPost', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
@@ -58,7 +58,7 @@ router.get('/profile', withAuth, async (req, res) => {
 
     const user = userData.get({ plain: true });
 
-    res.render('profile', {
+    res.render('jobsPost', {
       ...user,
       logged_in: true
     });
@@ -70,7 +70,7 @@ router.get('/profile', withAuth, async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/jobsPost');
     return;
   }
 
